5a632f1d963bcc564e132e3fa68a9036a4d1ec94
iAmGjert <gjertsen.eric@gmail.com>
2021-12-08 01:31:03 +0000

    finished FizzBuzz

--- projects/scratch-pad/day-2/homework/fizz-buzz.js
+++ projects/scratch-pad/day-2/homework/fizz-buzz.js
@@ -12,8 +12,19 @@
 */
 function fizzBuzz() {
     // YOUR CODE GOES BELOW HERE //
-
-
+//create a for loop starting at 1 and ending at 100
+    for (var i = 1; i <= 100; i++){
+        //check if the number i'm currently at is divisible by both 5 and 3
+        if ((i % 3 === 0) && (i % 5 === 0)){
+            console.log("FizzBuzz");//if true print FizzBuzz
+        } else if (i % 3 === 0){//check if divisible by just 3
+            console.log("Fizz");//if true print Fizz
+        } else if (i % 5 === 0){//check if divisible by just 5
+            console.log("Buzz");//if true print Buzz
+        } else {
+            console.log(i);//if all else fails print number
+        }
+    }
 
     // YOUR CODE GOES ABOVE HERE //
 }
